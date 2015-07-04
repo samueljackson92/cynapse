6bcfdf128a6f54fa6e0fea76914a6a1757afe9ba
Samuel Jackson <samueljackson@outlook.com>
2015-07-04 16:22:45 +0100

    Update test framework. Fill out NeuralNetwork test.

--- src/test/NeuralNetworkTest.cpp
+++ src/test/NeuralNetworkTest.cpp
@@ -1,13 +1,59 @@
 
-#define CATCH_CONFIG_MAIN
+#include <Eigen/Dense>
 #include <vector>
 
-#include "../catch.hpp"
+#include "TestsCatchRequire.h"
 #include "../NeuralNetwork.h"
 
-TEST_CASE( "NeuralNetwork can feed forward", "[NeuralNetwork]" ) {
-
+TEST_CASE("NeuralNetwork can be initialised", "[NeuralNetwork]") {
+    // create a basic layout 2 inputs, 2 nodes in the 1st layer 1 in the 2nd
     int nodes[3] = { 2, 2, 1};
     std::vector<int> layout(&nodes[0], &nodes[0]+3);
 
+    SECTION("Creating a network with valid parameters") {
+        REQUIRE_NOTHROW(NeuralNetwork(layout, "step"));
+    }
+
+    SECTION("Creating a network with sigmoid function") {
+        REQUIRE_NOTHROW(NeuralNetwork(layout, "sigmoid"));
+    }
+
+    SECTION("Creating a network with invalid function") {
+        REQUIRE_THROWS(NeuralNetwork(layout, "somefunc"));
+    }
+
+    SECTION("Creating a network without a random seed") {
+        REQUIRE_NOTHROW(NeuralNetwork(layout, "step", false));
+    }
+}
+
+TEST_CASE("Testing NeuralNetwork feedForward", "[NeuralNetwork]") {
+    // create a basic layout 2 inputs, 2 nodes in the 1st layer 1 in the 2nd
+
+    SECTION("Check feedForward with valid parameters") {
+        int nodes[3] = { 2, 2, 1};
+        std::vector<int> layout(&nodes[0], &nodes[0]+3);
+
+        NeuralNetwork ann(layout, "step", false);
+
+        Eigen::Vector2d input(1, 1);
+        Eigen::VectorXd output = ann.feedForward(input);
+
+        REQUIRE(output.size() == 1);
+        REQUIRE(output.value() == 0);
+    }
+
+    SECTION("Check feedForward with multiple outputs") {
+        int nodes[3] = { 3, 2, 2};
+        std::vector<int> layout(&nodes[0], &nodes[0]+3);
+
+        NeuralNetwork ann(layout, "sigmoid", false);
+
+        Eigen::Vector3d input(1, 1, 1);
+        Eigen::VectorXd output = ann.feedForward(input);
+
+        REQUIRE(output.size() == 2);
+        REQUIRE(output[0] == Approx(0.442010));
+        REQUIRE(output[1] == Approx(0.464295));
+    }
 }
---
+++ src/test/TestsCatchRequire.h
@@ -0,0 +1,7 @@
+
+/* Catch requires CATCH_CONFIG_MAIN to be defined *exactly* once. This file
+ includes the define statement and imports the header file. This prevents the
+ recompilation catch for every test case.
+*/
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
